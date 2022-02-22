193c66540b1da107aed441a66dd5309780c9fada
lautarog1596 <lautarog1596@gmail.com>
2022-02-22 13:40:20 -0300

    Get Query Parameter Input from the Client

--- myApp.js
+++ myApp.js
@@ -58,5 +58,10 @@ app.get("/:word/echo", (req, res) => {
   res.json({ echo: req.params.word });
 })
 
+// 9) Get Query Parameter Input from the Client
+app.get("/name", (req, res) => {
+  res.json({ name: req.query.first + " " + req.query.last });
+})
+
 
  module.exports = app;
